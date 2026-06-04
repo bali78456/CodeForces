1531da10718996d05876969112eb586e6b975e4e
bali78456 <bali78456@naver.com>
2026-06-04 19:08:06 +0900

    Solve Div2_309 A

---
+++ Div2_309/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    int sum = 0;
+    for(int i = 0; i < s.length()+1;i++)
+        sum += 25;
+    cout << sum + 1;
+    return 0;
+}
+
+// 26개 알파벳 중복때문에 + 1 마지막에
+// s.len=1 앞 뒤로 한번씩 25 + 25 + 1
+// s.len=2 라면 사이사이 총 세번 25+25+25+1
