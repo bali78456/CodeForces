758be17b12ba8a72f33827a475229952ef22bf66
bali78456 <bali78456@naver.com>
2026-06-27 14:34:31 +0900

    Solve ABC422 C

--- AtCoder/ABC/422/C.cpp
+++ AtCoder/ABC/422/C.cpp
@@ -10,8 +10,7 @@ void solve()
     {
         ll a, b, c;
         cin >> a >> b >> c;
-
-        
+        cout << min(a, min(c, (a + b + c) / 3))<<"\n";
     }
 }
 
@@ -28,5 +27,11 @@ int main() {
 // ABC -> A 카드 1개, B 카드 1개, C 카드 1개 사용
 // ACC -> A 카드 1개, C 카드 2개 사용
 // 3 종류 대회 개최 가능
+
+// 상한선
+// 1. 총 횟수는 A를 넘을 수 없다
+// 2. 총 횟수는 C를 넘을 수 없다
+// 3. 뭘 하든 총 3장의 카드가 쓰이니까 (A+B+C)/3을 못넘는다
+// => X = min(A,C,(A+B+C)/3)
 
 // https://atcoder.jp/contests/abc422/tasks/abc422_c
